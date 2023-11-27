bc582d2c28c2d46f239af1ba90fea582597381e5
ravePinchas <57952952+ravePinchas@users.noreply.github.com>
2023-11-27 17:49:32 +0200

    Initial StaysTypeBar style

--- src/pages/StayIndex.jsx
+++ src/pages/StayIndex.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import StaysTypeBar from '../cmp/StayIndex/StaysTypeBar'
 
 
 export default function StayIndex() {
   return (
-    <>
-    </>
+    <section>
+      <StaysTypeBar></StaysTypeBar>
+    </section>
   )
 }
